7677348db8a145dabbf575394d40edb7e08c07c5
sainath28220 <92886694+sainath28220@users.noreply.github.com>
2023-02-07 22:52:17 +0530

    1 file modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
+import { useState } from 'react';
 import Accordion from './components/Accordion'
 
+
 function App() {
+  const [ index,setIndex ] = useState(0);
+  const handleClick = (id) => {
+    if (id===index){
+      setIndex(0)
+    }else{
+      setIndex(id)
+    }
+  }
   const items = [{
     id:1,
     label: 'can i use javascript on a project?',
@@ -14,7 +24,9 @@ function App() {
     label: 'can i use ruby on rails on a project?',
     content: "You can use javascript on any project u want to work on in any project"
   }]
-  return <Accordion items={items}/>
+  return( 
+    <Accordion items={items}/>
+  )
 }
 
 export default App;
